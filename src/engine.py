6402c7af1c103b9c877d2cddfa852bc9f489f173
neil <neilalbuquerque2002@gmail.com>
2024-01-13 00:01:31 +0100

    Create Engine class to represent db connection engine

---
+++ src/engine.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+
+class Engine:
+
+    def __init__(self, user: str, password: str, host: str, port: int, db_type: str, db_name: str):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.db_type = db_type
+        self.db_name = db_name
+
+        self.db_engine = create_engine(f"{db_type}://{user}:{password}@{host}:{port}/{db_name}")
+
+    def get_db_connection_url(self):
+        return f"{self.db_type}://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
