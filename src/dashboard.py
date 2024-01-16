7230946d01103b82a857ed7d93844f11e8340299
neil <neilalbuquerque2002@gmail.com>
2024-01-16 12:56:51 +0100

    Add tabs to dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -10,74 +10,45 @@ import requests
 
 app = Dash(external_stylesheets=[dbc.themes.CYBORG])
 
-def create_dropdown(options, id):
-    return html.Div([
-        html.H2(id.replace("-select", "").replace("-", " ").capitalize(), style={"padding" : "0px 30px 0px 30px"}), 
-        dcc.Dropdown(options, id=id, value=options[0])
-    ], style={"padding" : "0px 20px 0px 20px"})
-
 app.layout = html.Div([
 
     html.Div([
-        # Shows us all the options we can select from and we have a default "value" setup
-        create_dropdown(["btcusd", "ethusd", "xrpusd"], id="coin-select"),
-        create_dropdown(["60", "3600", "86400"], id="timeframe-select"),
-        create_dropdown(["20", "50", "100"], id="num-bars-select"),
-    ], style={"display" : "flex", "margin" : "auto", "width" : "1200px", "justify-content" : "space-around"}),
+        html.H1("Personal Dashboard", style={'text-align': 'center'})
+    ]),
 
-    
-    html.Div([
-        dcc.RangeSlider(0, 20, 1, value= [0, 20], id="range-slider")
-    ], id="range-slider-container", style={"width":"1200px", "margin":"auto", "padding-top":"30px"}),
-    dcc.Graph(id="candles"), 
-    dcc.Interval(id="interval", interval=2000)
+    dcc.Tabs(id="tabs", value="real-time-stocks", children=[
+        dcc.Tab(label="Real Time Stocks Tracker", value="real-time-stocks"), 
+        dcc.Tab(label="News", value="news"), 
+        dcc.Tab(label="Weather", value="weather"), 
+        dcc.Tab(label="Exchange Rates", value="exchange-rates")
+    ]),
+
+    html.Div(id="current-tab"),
 ])
 
+@app.callback(Output("current-tab", "children"),
+              Input("tabs", "value"))
+def render_content(tab):
+    if tab == "real-time-stocks":
+        return html.Div([
+            html.H3("Real Time Stock Tracker")
+        ])
+    
+    elif tab == "news":
+        return html.Div([
+            html.H3("News")
+        ])
 
+    elif tab == "weather":
+        return html.Div([
+            html.H3("Weather")
+        ])
 
-@app.callback(Output("range-slider-container", "children"), Input("num-bars-select", "value"))
-def update_range_slider(num_bars):
-    return dcc.RangeSlider(min=0, max=int(num_bars), step=int(int(num_bars)/20), value=[0, int(num_bars)], id="range-slider")
-
-# interval is the id of the interval we had above, and n_interval indicates the number of intervals which have passed which we can see in the Interval documentation
-# callback decorators allows our function to be called when something happens to the input id object
-
-# Basically first parameter is the id and the second parameter is the particular attribute we want from that object with the particular id
-@app.callback(Output("candles", "figure"), Input("interval", "n_intervals"), Input("coin-select", "value"), Input("timeframe-select", "value"), Input("num-bars-select", "value"), Input("range-slider", "value"))
-def update_figure(n_intervals, coin_pair, timeframe, num_bars, range_values):
-
-    url = f"https://www.bitstamp.net/api/v2/ohlc/{coin_pair}"
-
-    params = {
-        "step" : timeframe, 
-        "limit" : num_bars
-    }
-
-    data = requests.get(url, params=params).json()["data"]["ohlc"]
-
-    data = pd.DataFrame(data)
-
-    data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
-
-    data = data.iloc[range_values[0]:range_values[1]]
-
-    candles = go.Figure(
-        data = [
-            go.Candlestick(
-                x =  data.timestamp,
-                open = data.open, 
-                high = data.high,
-                low = data.low,
-                close = data.close
-            )
-        ]
-    )
-
-    candles.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark")
-
-
-    return candles
-
+    elif tab == "exchange-rates":
+        return html.Div([
+            html.H3("Exchange Rates")
+        ])
+        
 
 if __name__ == "__main__":
     app.run_server(debug=True)
